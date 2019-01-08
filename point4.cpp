1843ab4b818af7d58d3a76c1072a8f76e070630e
amrithabastin <42172952+amrithabastin@users.noreply.github.com>
2019-01-08 10:37:53 +0000

    Update point4.cpp

--- point4.cpp
+++ point4.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main(){
-//declare variables
-int a, b , sum ;
-//ask for values
-cout<<"enter first number"<<endl;
-cin>>a;
-cout<<"enter second number"<<endl;
-cin>>b;
-//pointers
-int*p=&a ;
-int *q=&b;
-//add
-sum = *q+*p ;
-//print
-cout<<" the required sum is "<<sum<<endl;
-return 0;
+  //declare variables
+  int a, b , sum ;
+  //ask for values
+  cout<<"enter first number"<<endl;
+  cin>>a;
+  cout<<"enter second number"<<endl;
+  cin>>b;
+  //pointers
+  int*p=&a ;
+  int *q=&b;
+  //add
+  sum = *q+*p ;
+  //print
+  cout<<" the required sum is "<<sum<<endl;
+  return 0;
 }
