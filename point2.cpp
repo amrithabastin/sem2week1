8ae19ae00be94202e0fbeeb26514c36ff177ff20
amrithabastin <42172952+amrithabastin@users.noreply.github.com>
2019-01-08 10:36:59 +0000

    Update point2.cpp

--- point2.cpp
+++ point2.cpp
@@ -4,26 +4,26 @@
 using namespace std;
 
 int main(){
-//declare variable
-int m=29;
-//print
-cout<<"m stores :"<<m<<endl;
-cout<<"&m stores :"<<&m<<endl;
-//use pointers
-int* ab=&m;
-//print
-cout<<"ab stores :"<<ab<<endl;
-cout<<"*ab stores :"<<*ab<<endl;
-//chage variable
-m=34;
-//print
-cout<<"now ab stores :"<<ab<<endl;
-cout<<"now *ab stores :"<<*ab<<endl;
-//change var using pointer
-*ab=78;
-//print
-cout<<"now &m stores :"<<&m<<endl;
-cout<<"now m stores :"<<m<<endl;
+  //declare variable
+  int m=29;
+  //print
+  cout<<"m stores :"<<m<<endl;
+  cout<<"&m stores :"<<&m<<endl;
+  //use pointers
+  int* ab=&m;
+  //print
+  cout<<"ab stores :"<<ab<<endl;
+  cout<<"*ab stores :"<<*ab<<endl;
+  //chage variable
+  m=34;
+  //print
+  cout<<"now ab stores :"<<ab<<endl;
+  cout<<"now *ab stores :"<<*ab<<endl;
+  //change var using pointer
+  *ab=78;
+  //print
+  cout<<"now &m stores :"<<&m<<endl;
+  cout<<"now m stores :"<<m<<endl;
 
-return 0;
+  return 0;
 }
